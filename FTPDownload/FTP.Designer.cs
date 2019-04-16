eb17878349a346b296f9a0396c6682d170163616
ngubenkov <38397469+ngubenkov@users.noreply.github.com>
2019-04-16 16:28:25 +0300

    first commit
    First commit. FTP downloader

---
+++ FTPDownload/FTP.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace FTPDownload
+{
+    partial class FTP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.cbCredentials = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(501, 120);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 5;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(60, 122);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(435, 20);
+            this.txtLink.TabIndex = 4;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(60, 176);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(516, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(57, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(59, 13);
+            this.lblInfo.TabIndex = 7;
+            this.lblInfo.Text = "Credentials";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Login:";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(114, 45);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(302, 45);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(240, 52);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 10;
+            this.lblPassword.Text = "Password:";
+            // 
+            // cbCredentials
+            // 
+            this.cbCredentials.AutoSize = true;
+            this.cbCredentials.Location = new System.Drawing.Point(438, 48);
+            this.cbCredentials.Name = "cbCredentials";
+            this.cbCredentials.Size = new System.Drawing.Size(99, 17);
+            this.cbCredentials.TabIndex = 3;
+            this.cbCredentials.Text = "Use credentials";
+            this.cbCredentials.UseVisualStyleBackColor = true;
+            this.cbCredentials.CheckedChanged += new System.EventHandler(this.cbCredentials_CheckedChanged);
+            // 
+            // FTP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 257);
+            this.Controls.Add(this.cbCredentials);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.btnDownload);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FTP";
+            this.Text = "FTP Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.CheckBox cbCredentials;
+    }
+}
+
---
+++ FTPDownload/FTP.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Mime;
+using System.Diagnostics;
+
+namespace FTPDownload
+{ 
+    public partial class FTP : Form
+    {
+        private WebRequest sizeRequest  ; // get size of file( sometimes events doesn't provide size
+        private WebRequest request; // downloading request
+        private NetworkCredential credentials; // for credentials
+
+        public FTP()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e) // download btn
+        {
+            if (string.IsNullOrEmpty(txtLink.Text)) // check for input
+            {
+                MessageBox.Show("Please enter url");
+            }
+            else // if user entered something -> go execution
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog(); // open dialog to save file
+                saveFileDialog.FileName = getFileName(txtLink.Text); // insert default file name from link
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK) // if path selected beging downloading
+                {
+                    Task.Run(() => downloadFTP(txtLink.Text, Path.GetFullPath(saveFileDialog.FileName).ToString())); // run downloading
+                }
+            }              
+        }
+
+        private void downloadFTP(string url, string path) // downloading
+        {
+            try
+            {
+                long sizeOfFile;        // size of file
+                int hugeFileHelper = 1; // used for convertion of big files
+
+                sizeRequest = WebRequest.Create(url); // get size of file( sometimes events doesn't provide size
+                request = WebRequest.Create(url); // downloading request
+
+                if (cbCredentials.Checked) // use credentials
+                {
+                    sizeRequest.Credentials = credentials;
+                    request.Credentials = credentials;
+                }
+
+                // get size of file manually, sometimes automatically it doesn't work
+                sizeRequest.Method = WebRequestMethods.Ftp.GetFileSize;
+                if (sizeRequest.GetResponse().ContentLength > int.MaxValue) // check if it's huge file -> do convertion
+                {
+                    hugeFileHelper = 2048; // change this value for really huge files
+                    sizeOfFile = (int)((long)sizeRequest.GetResponse().ContentLength / hugeFileHelper);
+                }
+                else // if file size can be fitted in int -> move on
+                {
+                    sizeOfFile = (int)sizeRequest.GetResponse().ContentLength;
+                }
+
+                progressBar.Invoke(     // set max value(size of file) of progress bar
+                    (MethodInvoker)(() => progressBar.Maximum = (int)(sizeOfFile)));
+
+                // Download the file
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+                using (Stream ftpStream = request.GetResponse().GetResponseStream())
+                using (Stream fileStream = File.Create(path))
+                {
+                    byte[] buffer = new byte[163840]; // in case of memory/speed problems reduce this value
+                    int read;
+                    while ((read = ftpStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        fileStream.Write(buffer, 0, read);
+                        long position = (long)fileStream.Position;
+                        progressBar.Invoke(
+                            (MethodInvoker)(() => progressBar.Value = (int)(position / hugeFileHelper))); // update progress bar
+                    }
+                }
+                progressBar.Invoke((MethodInvoker)(() => progressBar.Value = 0)); // set progress as 0 when finish
+                MessageBox.Show("Download finished");
+            }
+            catch (WebException e)
+            {
+                if (e.Message == "The remote server returned an error: (530) Not logged in.") // Unauthorized access
+                    MessageBox.Show("Use correct credentials to log in");
+
+                else if (e.Message == "The remote server returned an error: (550) File unavailable (e.g., file not found, no access).") // no file or no access to file
+                    MessageBox.Show("File unavailable (e.g., file not found, no access)");
+
+                else // other error cases
+                    MessageBox.Show(e.Message);
+            }
+            catch (Exception e)
+            {
+                if (e.Message == "Invalid URI: The format of the URI could not be determined.") // incorrect url
+                    MessageBox.Show("Use correct correct url");
+
+                else // other error cases
+                    MessageBox.Show(e.Message);
+            }
+        }
+
+        private string getFileName(string url) // return file name based on url
+        {
+            return url.Substring(url.LastIndexOf("/") + 1);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbCredentials_CheckedChanged(object sender, EventArgs e)  // use credentials for downloading
+        {
+            if (cbCredentials.Checked)
+            {
+                credentials = new NetworkCredential(txtLogin.Text, txtPassword.Text); 
+                txtLogin.ReadOnly = true;
+                txtPassword.ReadOnly = true;
+            }
+            else
+            {
+                txtLogin.ReadOnly = false;
+                txtPassword.ReadOnly = false;
+            }
+        }
+    }
+}
